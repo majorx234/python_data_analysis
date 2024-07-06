c379570a92ec899342b9644d92b29ec1f36f6fd8
MajorX234 <majorx234@googlemail.com>
2024-07-06 19:28:48 +0200

    filter and transform data to plot

--- eurostat_data_visualization.py
+++ eurostat_data_visualization.py
@@ -10,3 +10,12 @@ df = df.rename(columns={'geo\TIME_PERIOD': "country"})
 df.columns = [datetime.strptime(f.split("-")[0] + "-" + f.split("-")[1] + "-01", "%Y-%m-%d")
               if f.startswith("19") or f.startswith("20") else f
               for f in df.columns]
+
+# filter out Data from Germany and Sweden
+se_vs_de_df = df[(df["country"].isin(["DE", "EA20", "SE"])) & (df["indic"] == "BS-CSMCI") & (df["s_adj"] == "NSA")]
+
+# transpose data and take country as index
+se_vs_de_df_only_data = se_vs_de_df[[c for c in df.columns if (isinstance(c, datetime) and c.year>=2000) or c == "country"]].set_index("country").transpose()
+
+se_vs_de_df_only_data.index = pd.DatetimeIndex(se_vs_de_df_only_data.index)
+se_vs_de_df_only_data.plot()
