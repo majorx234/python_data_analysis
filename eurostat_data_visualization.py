344eb87396ee0f510cd3f9160ec7870a2e5888a0
MajorX234 <majorx234@googlemail.com>
2024-07-07 14:44:17 +0200

    Add bar plot

--- eurostat_data_visualization.py
+++ eurostat_data_visualization.py
@@ -18,7 +18,27 @@ se_vs_de_df = df[(df["country"].isin(["DE", "EA20", "SE"])) & (df["indic"] == "B
 # transpose data and take country as index
 se_vs_de_df_only_data = se_vs_de_df[[c for c in df.columns if (isinstance(c, datetime) and c.year>=2000) or c == "country"]].set_index("country").transpose()
 
+# change index transform datetime-obj into datetime64
 se_vs_de_df_only_data.index = pd.DatetimeIndex(se_vs_de_df_only_data.index)
+# plot normal curve and a flatted one
 se_vs_de_df_only_data.plot()
-se_vs_de_df_only_data.resample("Q").mean().plot()
+se_vs_de_df_only_data.resample("QE").mean().plot()
+
+# Barplot consum index germany 2023
+de_cci_22or23_df = df[(df["country"] == "DE")
+                      & (df["indic"] == "BS-CSMCI")
+                      & (df["s_adj"] == "NSA")]
+de_cci_22or23_df_data = de_cci_22or23_df[[c for c in df.columns
+                                          if (isinstance(c, datetime)
+                                              and (c.year == 2022
+                                                   or c.year == 2023))]]
+de_cci_22or23_df_data.index = ["Consumer confidence indicator"]
+
+# transpose date
+de_cci_22or23_df_data_t = de_cci_22or23_df_data.transpose()[::-1]
+# change index transform datetime-obj into datetime64
+de_cci_22or23_df_data_t.index = pd.DatetimeIndex(de_cci_22or23_df_data_t.index)
+de_cci_22or23_df_data_t.plot.bar(figsize=(16, 10))
+
+# plot graphs
 plt.show()
